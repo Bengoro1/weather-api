1e35fb76eaeead1cece5cf4297fc1d07174da076
Benjamin <benjamin.barvas@gmail.com>
2023-10-23 11:08:48 +0200

    print location data

--- src/get-data.js
+++ src/get-data.js
@@ -3,7 +3,7 @@ import page from './page.js';
 
 async function getData(location) {
   try {
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=073602bf8ff34b64bdf213224231010&q=${location}`, {mode: 'cors'});
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=073602bf8ff34b64bdf213224231010&days=7&q=${location}`, {mode: 'cors'});
     const data = await response.json();
     obj = await data;
     page();
--- src/page.js
+++ src/page.js
@@ -2,23 +2,30 @@ import { obj } from './index.js';
 import getData from './get-data.js';
 
 export default function page() {
-  const container = document.createElement('div');
-  container.classList.add('container');
-  document.body.appendChild(container);
-  const input = document.createElement('input');
-  input.value = 'Bratislava';
-  container.appendChild(input);
+  if (!document.querySelector('.container')) {
+    const container = document.createElement('div');
+    container.classList.add('container');
+    document.body.appendChild(container);
+    const input = document.createElement('input');
+    input.value = 'Bratislava';
+    container.appendChild(input);
+    
+    const button = document.createElement('button');
+    container.appendChild(button);
+    button.textContent = 'click';
+    button.addEventListener('click', () => {
+      getData(input.value);
+    });
+  }
+  const container = document.querySelector('.container');
 
-  const button = document.createElement('button');
-  container.appendChild(button);
-  button.textContent = 'click';
-  button.addEventListener('click', () => {
-    getData(input.value);
-  });
-
-  const current = document.createElement('div');
-  container.appendChild(current);
-
+  const currentDay = document.createElement('div');
+  currentDay.classList.add('day-container');
+  container.appendChild(currentDay);
+  const location = document.createElement('div');
+  currentDay.appendChild(location);
+  location.textContent = `${obj.location.name} (${obj.location.region}) ${obj.location.country} ${obj.location.localtime}`;
+  
   console.log(obj);
   console.log(obj.forecast.forecastday[0].hour);
   const d = new Date();
